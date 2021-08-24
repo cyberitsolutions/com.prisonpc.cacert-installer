e230e53ab823e28d2068c5ef69e0d28ca830c9fc
Mike Abrahall <mike@abrahall.id.au>
2021-08-24 15:21:26 +1000

    Read 'Hello world' text from a resource file

--- app/src/main/java/com/prisonpc/cacertinstaller/MainActivity.java
+++ app/src/main/java/com/prisonpc/cacertinstaller/MainActivity.java
@@ -1,5 +1,15 @@
 package com.prisonpc.cacertinstaller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import android.widget.TextView;
+
 import android.app.Activity;
 import android.os.Bundle;
 
@@ -8,7 +18,28 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        final TextView textV = new TextView( MainActivity.this );
+
+        InputStream is = this.getResources().openRawResource(R.raw.textfile);
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        String readLine = null;
+        try {
+            // While the BufferedReader readLine is not null
+            while ((readLine = br.readLine()) != null) {
+                textV.setText(readLine);
+                // Log.d("TEXT", readLine);
+            }
+            // Close the InputStream and BufferedReader
+            is.close();
+            br.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        setContentView( textV );
+
+        // setContentView(R.layout.activity_main);
     }
 }
 
